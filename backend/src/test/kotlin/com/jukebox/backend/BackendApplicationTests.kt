d7b14bc533ebc9d5c76d650d649d5b9e49d19caf
Sori Lim <aint.imsorry@gmail.com>
2026-02-17 15:39:47 +0900

    feat: create backend spring app

---
+++ backend/src/test/kotlin/com/jukebox/backend/BackendApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.jukebox.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JukeboxApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
