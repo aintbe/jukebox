2a3b757a32592953c5438b21077528db2f0478a8
Sori Lim <aint.imsorry@gmail.com>
2026-03-05 03:03:57 +0900

    feat(be): add spotify controller in relay server; continued

--- backend/api/src/main/kotlin/com/jukebox/api/config/WebConfig.kt
+++ backend/api/src/main/kotlin/com/jukebox/api/config/WebConfig.kt
@@ -1,15 +1,15 @@
 package com.jukebox.api.config
 
-import com.jukebox.api.jukebox.web.JukeboxContextResolver
+import com.jukebox.api.jukebox.web.RequestContextResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    private val jukeboxContextResolver: JukeboxContextResolver,
+    private val requestContextResolver: RequestContextResolver,
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(jukeboxContextResolver)
+        resolvers.add(requestContextResolver)
     }
 }
